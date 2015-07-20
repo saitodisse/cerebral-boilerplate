e9f46046c3468880026ac3d593679fd6d31538c3
Julio Makdisse Saito <saitodisse@gmail.com>
2015-07-20 03:56:47 -0300

    get index, util tools 2

--- app/actions/setInputValueFromSelection.js
+++ app/actions/setInputValueFromSelection.js
@@ -1,27 +1,8 @@
-function getItemId(cerebral) {
-	var list_length = cerebral.get('list').length;
-	var uniqid = 'item_' + Number(list_length + 1);
-	return uniqid;
-}
+import getSelectedItem from './utils/getSelectedItem.js';
 
 let setInputValueFromSelection = function (cerebral) {
-
-  var all_list = cerebral.get('list');
-  var selected_item_id;
-
-  for(let i = 0; i < all_list.length; i++) {
-    let item = all_list[i];
-    if (item.isSelected) {
-      selected_item_id = i;
-      break;
-    }
-  }
-
-  if (typeof selected_item_id === 'undefined') {
-    return;
-  }
-
-  cerebral.set('inputValue', all_list[selected_item_id].text);
+  var selected_item = getSelectedItem(cerebral);
+  cerebral.set('inputValue', selected_item.text);
 };
 
 export default setInputValueFromSelection;
